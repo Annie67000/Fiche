819f37ecca844079e6a6a2de5838c3a262aeaadf
Annie <rahelinirinajulieannie@gmail.com>
2025-11-11 11:43:10 +0300

    feat: integrate Tailwind CSS into the frontend project and remove unused CSS styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Demo from './components/login.jsx';
 
 function App() {
   return (
-    <div>
+    <div className='bg-pink-700 h-screen flex items-center justify-center'>
       <Demo />
     </div>
   )
