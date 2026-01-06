c375b2ca5be5d09533d7d0717f2e7fb6d88821ab
Alain Nambii <alainnambi@gmail.com>
2026-01-06 13:06:56 +0100

    feat: enhance sidebar layout with scrollable area and improved structure

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -21,18 +21,18 @@ export default function Sidebar() {
 
   return (
     <Box w={280} className='bg-green-700' h="100vh" p="md" style={{ position: 'fixed', left: 0, top: 0 }}>
-      {/* Logo + Titre */}
-      <Box className='flex flex-col items-center justify-center' my="xl">
-        <Avatar size={80} radius={40} color="white" title='Fiche de Paie'>
-          <Text size={32} weight={700} color="white">FP</Text>
-        </Avatar>
-        {/* <Text size="lg" color="white" mt="md">Service RH</Text> */}
-      </Box>
+      <ScrollArea h="100%"  scrollbarSize={8}>
+        {/* Logo + Titre */}
+        <Box className='flex flex-col items-center justify-center' my="xl">
+          <Avatar size={80} radius={40} color="white" title='Fiche de Paie'>
+            <Text size={32} weight={700} color="white">FP</Text>
+          </Avatar>
+          {/* <Text size="lg" color="white" mt="md">Service RH</Text> */}
+        </Box>
 
-      <Divider color="white" opacity={0.2} my="lg" />
+        <Divider color="white" opacity={0.2} my="lg" />
 
-      {/* Menu */}
-      <ScrollArea h={350} mx="-md">
+        {/* Menu */}
         <Box mx="xs">
           {menuItems.map((item) => {
             const Icon = item.icon;
@@ -51,20 +51,19 @@ export default function Sidebar() {
             );
           })}
         </Box>
+
+        {/* Déconnexion */}
+        <Box>
+          <Divider color="white" opacity={0.2} my="lg" />
+          <Link
+            to={'/login'}
+            className={`flex items-center gap-2 px-6 py-4 mb-1 rounded-md text-md text-white hover:bg-red-500 hover:bg-opacity-10`}
+          >
+            <IconLogout size={24} />
+            <span className=''>Déconnexion</span>
+          </Link>
+        </Box>
       </ScrollArea>
-
-
-      {/* Déconnexion */}
-      <Box>
-        <Divider color="white" opacity={0.2} my="lg" />
-        <Link
-          to={'/login'}
-          className={`flex items-center gap-2 px-6 py-4 mb-1 rounded-md text-md text-white hover:bg-red-500 hover:bg-opacity-10`}
-        >
-          <IconLogout size={24} />
-          <span className=''>Déconnexion</span>
-        </Link>
-      </Box>
     </Box>
   );
 }
