76a8023fbe963c06b0fa775522b916111584023d
Alain Nambii <alainnambi@gmail.com>
2026-04-29 14:34:20 +0300

    feat: add Docker Compose setup for PostgreSQL and services fix: update permissions for various files in pdf_separate_intelligent

--- command.bat
+++ command.bat
@@ -1,2 +1,4 @@
 docker exec -it payslip_backend bash -c "python manage.py seed_employees"
 docker exec -it payslip_backend bash -c "python manage.py createsuperuser"
+chmod -R 755 pdf_separate_intelligent
+chmod -R 777 pdf_separate_intelligent
